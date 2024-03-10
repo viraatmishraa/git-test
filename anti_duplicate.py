af7c2c830d69b7a1f38e36a90290ccb3e37ebb12
Virat Mishra <viraatmishra007@gmail.com>
2024-03-10 19:52:32 +0530

    New Implementation added

--- anti_duplicate.py
+++ anti_duplicate.py
@@ -1,26 +1,13 @@
-def duplicheck():
-    n=input("enter the size of array: ")
-    if not n:
-        print("size is not given \n go again \n")
-        duplicheck()
+n = ""
+while n == "":
+    n = input("Enter size or arr : ")
 
-    try: 
-        n=int(n)
-    except Exception as e:
-        print(e)
-        print("go again\n")
-        duplicheck()
+n = int(n)
 
-    if n <= 0:
-        print("empty array \n go again\n")   
-        duplicheck()
-    arr=[]
-    set1={}
-    for i in range (0,n):
-        inp=input(f"enter the element {i+1}: ")
-        arr.append(inp)
-    set1=set(arr)
-    array=list(set1)
-    print(f"new array={array}")
+arr = []
+for i in range(0, n):
+    element = int(input(f"Enter the {i+1}th Element : "))
+    arr.append(element)
 
-duplicheck()
+newList = list(set(arr))
+print(newList)
